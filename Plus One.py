fbae5efdabad8c866415205d0706af26e41a6903
Cloris <hlyy1994>
2020-10-31 22:32:51 -0400

    Plus One

---
+++ Plus One.py
@@ -0,0 +1,21 @@
+'''
+Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+'''
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        for i in range(n):
+            idx = n - 1 - i
+            if digits[idx] == 9:
+                digits[idx] = 0
+            else:
+                digits[idx] += 1
+                return digits
+        return [1] + digits
